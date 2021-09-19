9a80be6a058dd729f79357cf524e0a69f6c4c943
Camden <camdenmorgan01@gmail.com>
2021-09-19 16:48:41 -0400

    Lab 2 HW

--- app/src/main/java/com/example/hellotoast/MainActivity.java
+++ app/src/main/java/com/example/hellotoast/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.hellotoast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -25,7 +26,23 @@ public class MainActivity extends AppCompatActivity {
 
     public void countUp(View view) {
         mCount++;
+        View zero_button = findViewById(R.id.zero_button);
+        if (mShowCount != null){
+            mShowCount.setText(Integer.toString(mCount));
+            zero_button.setBackgroundColor(Color.CYAN);
+        }
+        if(mCount % 2 == 1){
+            view.setBackgroundColor(Color.GREEN);
+        }
+        else{
+            view.setBackgroundColor(Color.BLUE);
+        }
+    }
+
+    public void zero(View view) {
+        mCount = 0;
         if (mShowCount != null)
             mShowCount.setText(Integer.toString(mCount));
+        view.setBackgroundColor(getResources().getColor(R.color.grey));
     }
 }
